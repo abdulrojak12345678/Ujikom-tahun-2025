3fe9daffcbadc37a1b2f658ea05f5f76a55d069a
abdulrojak12345678 <rojaka140507@gmail.com>
2025-04-28 09:29:07 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
