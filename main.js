136d95580948d21f9d5734ec9623d913a7fe6fc7
abdulrojak12345678 <rojaka140507@gmail.com>
2025-04-28 09:36:08 +0700

    menambahkan konfigurasi firebase

--- main.js
+++ main.js
@@ -11,4 +11,13 @@ import {
   orderBy,
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
+// conpigurasi firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBcym1i4oAyM2rFmBU_Ipa0vcC7Pdz0dws",
+  authDomain: "insan-cemerlang-2e18f.firebaseapp.com",
+  projectId: "insan-cemerlang-2e18f",
+  storageBucket: "insan-cemerlang-2e18f.appspot.com",
+  messagingSenderId: "1096016420480",
+  appId: "1:1096016420480:web:87611389fc765e7ddbd065",
+  measurementId: "G-DW23S2DXCR"
+};
